484a4a2bb47f9c78d835c04c9fac4663bdd1567c
prashishh <prashishh@gmail.com>
2013-06-16 12:20:08 +0545

    nodejs

---
+++ routes/api.js
@@ -0,0 +1,108 @@
+
+// responds list of all databases - beginning of setup
+exports.getall = function ( req, res ) {
+  
+  var mysql = require('mysql');
+  var output = [];
+
+  var connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    port     : '3306',
+    user     : 'root',
+    password : ''
+  });
+
+  connection.connect();
+  console.log('Connected to MySQL.');
+  res.writeHeader(200, {'Content-type': 'application/json'});
+
+  var output = [];
+  connection.query(';', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log("Query fired and successful.")
+    for (var i in rows) {
+        output.push(rows[i].Database);
+    }
+    var result = output.join(',');
+
+    console.log("Response: " + result);
+    
+    res.write(result);
+    res.end();
+  });
+  console.log("Connection Closed.");
+  connection.end();
+};
+
+// responds list of all databases - beginning of setup
+exports.getreportnumber = function ( req, res ) {
+  
+  var mysql = require('mysql');
+  var output = [];
+
+  var connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    port     : '3306',
+    user     : 'root',
+    password : ''
+  });
+
+  connection.connect();
+  console.log('Connected to MySQL.');
+  res.writeHeader(200, {'Content-type': 'application/json'});
+
+  var output = [];
+  connection.query(';', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log("Query fired and successful.")
+    for (var i in rows) {
+        output.push(rows[i].Database);
+    }
+    var result = output.join(',');
+
+    console.log("Response: " + result);
+    
+    res.write(result);
+    res.end();
+  });
+  console.log("Connection Closed.");
+  connection.end();
+};
+
+// responds list of all databases - beginning of setup
+exports.getdistrictnumber = function ( req, res ) {
+  
+  var mysql = require('mysql');
+  var output = [];
+
+  var connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    port     : '3306',
+    user     : 'root',
+    password : ''
+  });
+
+  connection.connect();
+  console.log('Connected to MySQL.');
+  res.writeHeader(200, {'Content-type': 'application/json'});
+
+  var output = [];
+  connection.query(';', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log("Query fired and successful.")
+    for (var i in rows) {
+        output.push(rows[i].Database);
+    }
+    var result = output.join(',');
+
+    console.log("Response: " + result);
+    
+    res.write(result);
+    res.end();
+  });
+  console.log("Connection Closed.");
+  connection.end();
+};
